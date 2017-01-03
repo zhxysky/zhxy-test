6d559cb4e0069bbd02a5ef9aa71a3cf6eef73194
zhxysky <9368036@qq.com>
2017-01-03 13:40:24 +0800

    add modules

---
+++ redis/src/main/java/com/zhxy/PipeLineTest.java
@@ -0,0 +1,10 @@
+package com.zhxy;
+
+/**
+ * Created by zhxy on 1/3/2017.
+ */
+public class PipeLineTest {
+    public static void main(String[] args) {
+        System.out.println("test....");
+    }
+}
